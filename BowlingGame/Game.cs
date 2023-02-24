2cfc7a48cb802c06837874acb6e8725b4309802f
Bruno Mario Veronesi <endeios@gmail.com>
2023-02-24 20:45:49 +0100

    refactor

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -13,18 +13,23 @@ public class Game
     public int Score()
     {
         var score = 0;
-        var i = 0;
+        var currentFrame = 0;
         
         for (var frame = 0; frame < 10; frame++)
         {
-            if (rolls[i]+rolls[i+1]==10)
+            if (isSpare(currentFrame))
             {
-                score += rolls[i+2];
+                score += rolls[currentFrame+2];
             }
-            score += rolls[i] + rolls[i+1];
-            i += 2;
+            score += rolls[currentFrame] + rolls[currentFrame+1];
+            currentFrame += 2;
         }
 
         return score;
+    }
+
+    private bool isSpare(int i)
+    {
+        return rolls[i]+rolls[i+1]==10;
     }
 }
