77818ed0576486311ec4383b2c145f1fdc115245
Bruno Mario Veronesi <endeios@gmail.com>
2023-02-24 19:58:43 +0100

    First Test

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -2,4 +2,12 @@
 
 public class Game
 {
+    public void Roll(int i)
+    {
+    }
+
+    public int Score()
+    {
+        return 0;
+    }
 }
--- BowlingGameTest/GameShould.cs
+++ BowlingGameTest/GameShould.cs
@@ -15,4 +15,16 @@ public class GameShould
         var game = new Game();
         Assert.That(game, Is.Not.Null);
     }
+
+    [Test]
+    public void SupportGutterGame()
+    {
+        var game = new Game();
+        for (int i = 0; i < 20; i++)
+        {
+            game.Roll(0);
+        }
+        
+        Assert.That(game.Score(), Is.EqualTo(0));
+    }
 }
